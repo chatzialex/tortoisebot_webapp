fff1a995bed52faa9d1b4a2f6464ff66dbb7367b
Alexandros Chatzikyriakos <chatzialex@yahoo.gr>
2024-09-29 14:06:40 +0000

    render map

--- main.js
+++ main.js
@@ -27,6 +27,10 @@ var app = new Vue({
         viewer: null,
         tfClient: null,
         urdfClient: null,
+        // map
+        mapViewer: null,
+        mapGridClient: null,
+        interval: null,
         // etc
         logs: [],
         loading: false,
@@ -45,6 +49,7 @@ var app = new Vue({
                 this.loading = false
                 this.setCamera()
                 this.setup3DViewer()
+                this.setupMapViewer()
             })
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
@@ -55,6 +60,7 @@ var app = new Vue({
                 this.loading = false
                 document.getElementById('divCamera').innerHTML = ''
                 this.unset3DViewer()
+                document.getElementById('map').innerHTML = ''
             })
         },
         disconnect: function() {
@@ -169,8 +175,34 @@ var app = new Vue({
         unset3DViewer() {
             document.getElementById('div3DViewer').innerHTML = ''
         },
+        // Map viewer
+        setupMapViewer () {
+            this.mapViewer = new ROS2D.Viewer({
+                divID: 'map',
+                width: 420,
+                height: 360
+            })
+
+            // Setup the map client.
+            this.mapGridClient = new ROS2D.OccupancyGridClient({
+                ros: this.ros,
+                rootObject: this.mapViewer.scene,
+                continuous: true,
+            })
+
+            // Scale the canvas to fit to the map
+            this.mapGridClient.on('change', () => {
+            this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
+            this.mapViewer.shift(this.mapGridClient.currentGrid.pose.position.x, this.mapGridClient.currentGrid.pose.position.y)
+            })
+        },
     },
     mounted() {
         window.addEventListener('mouseup', this.stopDrag)
+        this.interval = setInterval(() => {
+            if (this.ros != null && this.ros.isConnected) {
+             this.ros.getNodes((data) => { }, (error) => { })
+            }
+        }, 10000)
     },
 })
